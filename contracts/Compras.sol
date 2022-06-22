a3a6f8e3839c6bf6f95747e4a52fd8447defca6c
stephaniepazos89 <stephaniepazos89@gmail.com>
2022-06-22 18:26:31 -0300

    posible solucion

--- contracts/Compras.sol
+++ contracts/Compras.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.10;
+
+//import "./Usuarios.sol";
 
 contract Compras {
   enum Estado { ACTIVO, LECTURA, BOOTSTRAP }
@@ -8,11 +10,22 @@ contract Compras {
   mapping(uint => Compra) public compras;
   
   struct Compra {
-      uint _id;
+      uint _idCompra;
+      //Usuario usuarioComprador;
       address usuarioComprador;
       uint precio;
       string[] articulos;
   }
+
+   //struct Usuario {
+     // address _idUsuario;
+     // uint saldo;
+  //}
+
+  //modifier duenio(){
+    //require(msg.sender == compras[_id].usuarioComprador , "El usuario no es duenio de esa compra");
+    //_;
+  //}
 
   modifier bootstrap() {
     require(estado == Estado.BOOTSTRAP || estado == Estado.ACTIVO, "Solo puede agregar compras."); 
@@ -28,9 +41,25 @@ contract Compras {
     return compras[_id];
   }
 
+  //function obtenerUsuarioDeLaCompra(uint _id) public view returns (Usuario memory) { 
+    //return compras[_id].usuarioComprador;
+  //}
+
   function agregarCompra(address _comprador, uint _precio, string[] memory _articulos) public bootstrap {
     incrementarCompras();
     compras[compraCount] = Compra(compraCount, _comprador, _precio, _articulos);
+    //restarSaldo(compraCount);
+  }
+
+  //function restarSaldo(uint _idCompra) private view {
+    //Usuario memory usuario = obtenerUsuarioDeLaCompra(_idCompra);
+    //Compra memory compra = obtenerCompra(_idCompra);
+    //saldoInsuficiente(usuario.saldo, compra);
+    //usuario.saldo = usuario.saldo - compra.precio;  
+  //}
+
+  function saldoInsuficiente(uint saldo, Compra memory compra) private pure{
+    require(saldo >= compra.precio, "Saldo insuficiente");
   }
 
   function activar() public {
