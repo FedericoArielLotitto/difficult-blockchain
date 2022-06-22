8dd97f85105143566c4b5e58fd05ee41d7816c5e
FedericoArielLotitto <federicoariellotitto@gmail.com>
2022-06-21 23:01:07 -0300

    Obtiene compra del contrato a partir de un id

--- test/compras_test.js
+++ test/compras_test.js
@@ -1,16 +1,35 @@
 const Compras = artifacts.require("Compras");
 
-contract("Compras", function (accounts) {
-  let compraContrato;
+let compraContrato;
 
+// contract("Compras", async (accounts) => {
+//   console.log("acá llega 1");
+//   beforeEach('initialized', async () => {
+//       console.log("acá llega 2");
+//       compraContrato = await Compras.new();
+//       await compraContrato.agregarCompra(accounts[0], 100, ["Chalosse", "Chantelley"]);
+//     });
+
+//   it("Dado un id de compra, obtiene la compra en ese id."), async () => {
+//     const compraEsperada = await compraContrato.obtenerCompra(0);
+//     console.log(compraEsperada);
+//     assert.equal(compraEsperada._id, 0);
+//   };
+
+// });
+contract("Compras", async (accounts) => {
   before(async function () {
     compraContrato = await Compras.new();
-    await compraContrato.agregarCompra(accounts[0], 100, ["Chalosse", "Chantelley"]);
   });
 
-  it("Dado un id de compra, obtiene la compra en ese id."), async () => {
-    const compraEsperada = await compraContrato.obtenerCompra(0);
-    assert.equal(compraEsperada._id, 0);
-  };
+  describe("Dado un id de compra, obtiene la compra con ese id.", async () => {
+    before("Obtiene la compra 0", async () => {
+      await compraContrato.agregarCompra(accounts[0], 10, ["Chalosse", "Chantelley"]);
+    });
 
+    it("puede obtener la compra.", async () => {
+      const compraEsperada = await compraContrato.obtenerCompra(0);
+      assert.equal(compraEsperada._id, 0, "Debería ser la primera");
+    });
+  });
 });
