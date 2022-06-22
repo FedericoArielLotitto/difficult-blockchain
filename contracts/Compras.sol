5710356fa21b56db135455b5843039c04f21a5ac
stephaniepazos89 <stephaniepazos89@gmail.com>
2022-06-22 20:06:44 -0300

    se agrega saldo al usuario, se saca el promedio y se descomentaron los metodos

--- contracts/Compras.sol
+++ contracts/Compras.sol
@@ -8,24 +8,30 @@ contract Compras {
   Estado public estado;
   uint public compraCount;
   mapping(uint => Compra) public compras;
+  mapping(address => Usuario) public usuarios;
+
+  address owner = msg.sender;
+
+  constructor()public{
+    owner = msg.sender;
+  }
   
   struct Compra {
       uint _idCompra;
-      //Usuario usuarioComprador;
-      address usuarioComprador;
+      Usuario usuarioComprador;
       uint precio;
       string[] articulos;
   }
 
-   //struct Usuario {
-     // address _idUsuario;
-     // uint saldo;
-  //}
+   struct Usuario {
+      address _idUsuario;
+      uint saldo;
+  }
 
-  //modifier duenio(){
-    //require(msg.sender == compras[_id].usuarioComprador , "El usuario no es duenio de esa compra");
-    //_;
-  //}
+  modifier duenio(){
+    require(msg.sender == owner , "El usuario no es duenio de esa compra");
+    _;
+  }
 
   modifier bootstrap() {
     require(estado == Estado.BOOTSTRAP || estado == Estado.ACTIVO, "Solo puede agregar compras."); 
@@ -41,25 +47,42 @@ contract Compras {
     return compras[_id];
   }
 
-  //function obtenerUsuarioDeLaCompra(uint _idCompra) public view returns (Usuario memory) { 
-    //return compras[_idCompra].usuarioComprador;
-  //}
-
-  function agregarCompra(address _comprador, uint _precio, string[] memory _articulos) public bootstrap {
-    incrementarCompras();
-    compras[compraCount] = Compra(compraCount, _comprador, _precio, _articulos);
-    //restarSaldo(compraCount);
+  function obtenerUsuarioDeLaCompra(uint _idCompra) public view returns (Usuario memory) { 
+    return compras[_idCompra].usuarioComprador;
   }
 
-  //function restarSaldo(uint _idCompra) private view {
-    //Usuario memory usuario = obtenerUsuarioDeLaCompra(_idCompra);
-    //Compra memory compra = obtenerCompra(_idCompra);
-    //saldoInsuficiente(usuario.saldo, compra);
-    //usuario.saldo -= compra.precio;  
-  //}
+  function agregarCompra(Usuario memory _comprador, uint _precio, string[] memory _articulos) public bootstrap {
+    incrementarCompras();
+    compras[compraCount] = Compra(compraCount, _comprador, _precio, _articulos);
+    restarSaldo(compraCount);
+  }
+
+  function agregarSaldo(uint _saldo) public duenio {
+        require(_saldo > 0, "Entrega la tarasca.");
+        usuarios[msg.sender].saldo += _saldo;
+    }
+
+  function restarSaldo(uint _idCompra) private view {
+    Usuario memory usuario = obtenerUsuarioDeLaCompra(_idCompra);
+    Compra memory compra = obtenerCompra(_idCompra);
+    saldoInsuficiente(usuario.saldo, compra);
+    usuario.saldo -= compra.precio;  
+  }
 
   function saldoInsuficiente(uint saldo, Compra memory compra) private pure{
     require(saldo >= compra.precio, "Saldo insuficiente");
+  }
+
+  function calcularPromedio() public view returns(uint) {
+      uint importeTotalUsuario = 0;
+      uint count = 0;
+    for(uint i=0 ; i<compraCount ; i++){
+      if (compras[i].usuarioComprador._idUsuario == msg.sender){
+        importeTotalUsuario += compras[i].precio;
+        count++;
+      }
+    }
+    return importeTotalUsuario/count;
   }
 
   function activar() public {
